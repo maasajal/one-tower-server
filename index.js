0ed49885805ea084f8c68fa640510ffdedb4cb4b
Abdullah Sajal <59488317+sajal-se@users.noreply.github.com>
2024-06-01 14:27:09 +0300

    Create a database user and collection of data in mongodb then connected to the node.js server

--- index.js
+++ index.js
@@ -15,6 +15,42 @@ app.get("/", (req, res) => {
   res.send("Welcome to One Tower!");
 });
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jd9hrzt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+const run = async () => {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+
+    // Collections from the database here
+    const apartmentCollection = client.db("oneTower").collection("apartment");
+
+    app.get("/apartment", async (req, res) => {
+      const result = await apartmentCollection.find().toArray();
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+};
+run().catch(console.dir);
+
 app.listen(port, () => {
   console.log(`One Tower server running on port ${port}!`);
 });
