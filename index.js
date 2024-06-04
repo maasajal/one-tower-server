e8e8c4d17d61ef835946b527ad733df3c982ba4c
Abdullah Sajal <59488317+sajal-se@users.noreply.github.com>
2024-06-04 21:03:09 +0300

    isMember check from the users collection

--- index.js
+++ index.js
@@ -80,6 +80,16 @@ const run = async () => {
       }
       res.send({ admin });
     });
+    app.get("/is-member/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      let member = false;
+      if (user) {
+        member = user?.role === "member";
+      }
+      res.send({ member });
+    });
     app.patch("/users/member/:id", async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
