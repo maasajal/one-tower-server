ae50fba9a963527fda348b4fb04e00322d7325df
Abdullah Sajal <59488317+sajal-se@users.noreply.github.com>
2024-06-05 00:12:07 +0300

    JWT sign when user sign up and login then verify manage users in manage members page

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
+const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
@@ -55,8 +55,35 @@ const run = async () => {
       res.send(result);
     });
 
+    // JWT related API
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "5h",
+      });
+      // console.log(user, token);
+      res.send({ token });
+    });
+
+    // Middleware
+    const verifyToken = async (req, res, next) => {
+      console.log("inside verify token", req.headers.authorization);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: "unauthorized access" });
+      }
+      const token = req.headers.authorization.split(" ")[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: "unauthorized access" });
+        }
+        req.decoded = decoded;
+        next();
+      });
+    };
+
     // User related API
-    app.get("/users", async (req, res) => {
+    app.get("/users", verifyToken, async (req, res) => {
+      console.log("verify ");
       const result = await userCollection.find().toArray();
       res.send(result);
     });
